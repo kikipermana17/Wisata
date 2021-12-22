3946ed1d3cae20effce6cf033659c0ef68ed5b94
kikipermana17 <kiw170904@gmail.com>
2021-12-22 11:43:58 +0700

    npm

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,16 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(
+    [
+        'register' => false,
+    ]
+);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
