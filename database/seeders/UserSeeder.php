0cd4b791791403afa24b7e74882361e2b2631691
kikipermana17 <kiw170904@gmail.com>
2021-12-23 11:12:27 +0700

    Laratrust

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,11 +16,32 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        $admin = Role::create([
+            'name' => 'admin',
+            'display_name' => 'User Administrator',
+
+        ]);
+
+        $pengguna = Role::create([
+            'name' => 'pengguna',
+            'display_name' => 'User Biasa',
+
+        ]);
+
         $user = new User();
         $user->name = 'Kiki Permana';
         $user->email = 'kiki@gmail.com';
         $user->password = Hash::make('12345678');
         $user->save();
+
+        $pengunjung = new User();
+        $pengunjung->name = 'Pengunjung';
+        $pengunjung->email = 'pengunjung@gmail.com';
+        $pengunjung->password = Hash::make('12345678');
+        $pengunjung->save();
+
+        $user->attachRole($admin);
+        $pengunjung->attachRole($pengguna);
 
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,23 @@ Auth::routes(
 );
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => ['auth', 'role:admin']], function () {
+    Route::get('/', function () {
+        return 'Halaman Admin';
+    });
+
+    Route::get('profile', function () {
+        return 'Halaman profil Admin';
+    });
+});
+
+Route::group(['prefix' => 'pengguna', 'middleware' => ['auth', 'role:pengguna']], function () {
+    Route::get('/', function () {
+        return 'Halaman Pengguna';
+    });
+
+    Route::get('profile', function () {
+        return 'Halaman profil pengguna';
+    });
+});
